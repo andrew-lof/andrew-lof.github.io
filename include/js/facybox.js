6432cdeb7d06b893c7aa4ce0a4bcacc6e2997563
Andrew Lof <lofa@wsim146.pointclickcare.local>
2016-07-19 16:11:22 -0400

    debug images not loading

--- include/js/facybox.js
+++ include/js/facybox.js
@@ -283,8 +283,8 @@
     $('#facybox').find('.n, .close , .s, .w, .e, .nw, ne, sw, se').each(function() {
 		var img = new Image();
 		
-    img.src = $(this).css('background-image');
-    img.src = img.src.replace('url(','').replace(')','');
+    img = $(this).css('background-image');
+    img.src = img.replace('url(','').replace(')','');
     //img.src = $(this).css('background-image').replace(/url\((.+)\)/, '$1');
 
     console.log(img.src);
